08ba7cf07197e5a9740f19260a50320589c9f811
jtnavd <81948549+jtnavd@users.noreply.github.com>
2021-06-13 15:06:49 +0300

    settings update

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,24 @@
 from django.db import models
-from dateti import dateTime
+from datetime import datetime
+
 
 class User(models.Model):
-    user_id = models.IntegerField(blank = True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    creation_date = models.DateTimeField(default=datetime.now, blank = True)
+
+
+class Profile(models.Model):
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     phone = models.CharField(max_length=20)
-    creation_date  models.DateTimeField(default=datetime.now, blank = True)
+    creation_date = models.DateTimeField(default=datetime.now, blank = True)
+    steam_id = models.CharField(max_length=200)
 
+class Game(models.Model):
+    steam_id = models.ManyToManyField(Profile)
+    game_name = models.CharField(max_length=200)
+    creation_date = models.DateTimeField(default=datetime.now, blank = True)
 
--- gamehub/settings.py
+++ gamehub/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
     'pages',
     'accounts',
 ]
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.contrib.auth.backends.ModelBackend',
 ]
 
 ROOT_URLCONF = 'gamehub.urls'
