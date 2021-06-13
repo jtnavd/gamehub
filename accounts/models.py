e86871c05935bb4cb656160a0dcb48a832840dd8
jtnavd <81948549+jtnavd@users.noreply.github.com>
2021-06-13 12:21:36 +0300

    accounts models

--- accounts/models.py
+++ accounts/models.py
@@ -1,2 +1,12 @@
 from django.db import models
+from dateti import dateTime
+
+class User(models.Model):
+    user_id = models.IntegerField(blank = True)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    phone = models.CharField(max_length=20)
+    creation_date  models.DateTimeField(default=datetime.now, blank = True)
+
 
